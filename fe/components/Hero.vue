ca230e6bda82997c1e06e2849c94e4bdc6a1359f
Roman08121995 <roman.business0812@gmail.com>
2025-07-10 11:58:40 -0400

    fe update to vue

--- fe/components/Hero.vue
+++
@@ -1,9 +0,0 @@
-<!-- components/Hero.vue -->
-<template>
-  <section class="hero">
-    <div class="hero-content">
-      <h1>Security Analysis</h1>
-      <p>Twelve Labs Powered CCTV Footage Analysis and Detailed Reporting</p>
-    </div>
-  </section>
-</template>
